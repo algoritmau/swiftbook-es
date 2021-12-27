425685c1d0885cca9f893bca761f5d4b875d684d
Mauricio Paternina <spaceinvadev@icloud.com>
2021-12-27 18:32:15 -0500

    Refactor Callout.js

--- components/Callout.js
+++ components/Callout.js
@@ -3,16 +3,26 @@ import CalloutStyles from '../styles/Callout.module.css'
 
 const Callout = ({ type, children }) => {
   const calloutType = `callout--${type}`
+  let title
+
+  switch (type) {
+    case 'experiment':
+      title = 'Experimenta'
+      break
+    case 'important':
+      title = 'Importante'
+      break
+    default:
+      title = 'Nota'
+  }
 
   return (
     <div
       className={`${CalloutStyles.callout} ${
-        type === 'experiment' ? CalloutStyles[calloutType] : ''
+        type !== 'note' ? CalloutStyles[calloutType] : ''
       }`}
     >
-      <p className={CalloutStyles.callout__title}>
-        {type === 'note' ? 'Nota' : 'Experimenta'}
-      </p>
+      <p className={CalloutStyles.callout__title}>{title}</p>
       {children}
     </div>
   )
